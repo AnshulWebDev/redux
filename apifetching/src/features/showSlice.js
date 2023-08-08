20f223ef3fe29474209d003b133b8df9c901441f
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-08 15:08:02 +0530

    ..

---
+++ apifetching/src/features/showSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = { value: 0 };
+
+export const showSlice = createSlice({
+  name: "showData",
+  initialState,
+  reducers: {
+    addData: (state, action) => {},
+    showData: (state, action) => {},
+  },
+});
+
+export const { addData, showData } = showSlice.actions;
+export default showSlice.reducer;
