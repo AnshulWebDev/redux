ba1bc370e03f289941bd55ae9ead9517618fc9e7
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-08 18:59:11 +0530

    ..

--- apifetching/src/App.js
+++ apifetching/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllData } from "./features/getUserSlice";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const dispatch = useDispatch();
+  const datas = useSelector((state) => {
+    return state.app;
+  });
 
-export default App
+  
+  return (
+    <div className="app">
+      <h1>hello</h1>
+      <button onClick={() => dispatch(getAllData())}>Get Github users</button>
+      {
+        datas.loading?(<h1 className="">Loading....</h1>):(datas.users.map((ele)=>(
+          <li key={ele.id}>{ele.login} = <a href={ele.html_url} target="_blank">{ele.html_url}</a></li>
+        )))
+      }
+    </div>
+  );
+};
+
+export default App;
--- apifetching/src/app/store.js
+++ apifetching/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  gitUser  from "../features/getUserSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    app: gitUser,
+  },
 });
